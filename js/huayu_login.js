7a75819c6d783b3ecccaae3c41bc20020808d259
gejun <476377863@qq.com>
2019-06-27 16:47:41 +0800

    修改项目

--- js/huayu_login.js
+++ js/huayu_login.js
@@ -8,3 +8,14 @@ function check_uname (){
         msg1.innerHTML="格式错误！";
     }
 }
+
+function check_pwd(){
+    var $upwd=upwd.value;
+    if ($upwd==""){
+        msg2.innerHTML="用户密码不能为空";
+    }else if($upwd.length>=6 && $upwd.length<=12){
+        msg2.innerHTML="格式正确！";
+    }else{
+        msg2.innerHTML="格式错误！";
+    }
+}
