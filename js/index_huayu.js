b8bcddc15d71ad47a5b3ee9def851cc5df7821e2
gejun <476377863@qq.com>
2019-07-15 20:43:20 +0800

    xiugai

--- js/index_huayu.js
+++ js/index_huayu.js
@@ -16,46 +16,66 @@ window.onload=function(){
     // 底部大图和文字
     var left_ove=document.getElementById("left_ove");
     var right_ove=document.getElementById("right_ove");
+    // 底部商品服务
+    var foot_d1=document.getElementById("foot_d1");
+    var foot_d2=document.getElementById("foot_d2");
+    var foot_d3=document.getElementById("foot_d3");
+    var foot_d4=document.getElementById("foot_d4");
+    // 侧边导航栏
+    var right_cb_lan=document.getElementById("right_cb_lan");
     window.onscroll=function(){
         var scrollTop=document.body.scrollTop || document.documentElement.scrollTop;
         // 两张大图
-        if(scrollTop>=50){
+        if(scrollTop>=300){
             left_img.style="left:0px";
             left_img.style.opacity="1";
             right_img.style="right:0";
             right_img.style.opacity="1";
         }
         // 限时促销
-        if(scrollTop>=914){
+        if(scrollTop>=1300){
             left_img_djs.style="left:0px";
             left_img_djs.style.opacity="1";
             right_img_djs.style="right:0";
             right_img_djs.style.opacity="1";
         }
         // 今日上新
-        if(scrollTop>=460){
+        if(scrollTop>=900){
             for(var img of left_huayi_1){
                 img.style="top:0px";
                 img.style.opacity="1";
             }
         }
         // 浪漫你我
-        if(scrollTop>=1500){
+        if(scrollTop>=1800){
             le_ri_low.style="left:0px";
             le_ri_low.style.opacity="1";
             le_ri_right.style="right:0px";
             le_ri_right.style.opacity="1";
         }
         // 装点家园
-        if(scrollTop>=2062){
+        if(scrollTop>=2600){
             for(var top of bottom_top){
                 top.style.transform="translateY(0)";
             }
         }
         // 底部大图和文字
-        if(scrollTop>=2700){
+        if(scrollTop>=3000){
             left_ove.style.transform="translateX(0)";
             right_ove.style.transform="translateX(0)";
+        }
+        // 底部商品服务
+        if(scrollTop>=3300){
+            foot_d1.style.transform="translateY(0)";
+            foot_d2.style.transform="translateY(0)";
+            foot_d3.style.transform="translateY(0)";
+            foot_d4.style.transform="translateY(0)";
+        }
+        // 侧边导航栏
+        if(scrollTop<700){
+            right_cb_lan.style="opacity:0 ;";
+        }else if(scrollTop>=700){
+            right_cb_lan.style="opacity:1 ;";
         }
     }
 }
