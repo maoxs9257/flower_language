05ac3caf6cc33f3b5a57586fc93f6804e1aab89f
gejun <476377863@qq.com>
2019-07-18 20:46:51 +0800

    xiugai

--- js/huayu_prodcut.js
+++ js/huayu_prodcut.js
@@ -6,8 +6,8 @@ console.log(body_top_pro);
 // 绑定事件处理函数
 body_top_pro.onclick=function(e){
     var btn=e.target;
-    console.log(btn.hasAttribute("jiaru_gw"));
-    if(!btn.hasAttribute("jiaru_gw")){
+    console.log(btn.className=="jiaru_gw");
+    if(btn.className=="jiaru_gw"){
         // 查询要修改的元素
     var gouwu_right=document.getElementsByClassName("gouwu_right")[0];
     
@@ -15,4 +15,15 @@ body_top_pro.onclick=function(e){
     gouwu_right.style.transform="translateX(0)";
     } 
 };
+// 单击购物车X关闭购物车侧边栏
+// 查找事件触发元素
+var back_img=document.getElementsByClassName("back_img")[0];
+// 绑定事件处理函数
+back_img.onclick=function(){
+    // 查找要修改的元素
+    var gouwu_right=document.getElementsByClassName("gouwu_right")[0];
+    // 修改元素
+    gouwu_right.style.transform="translateX(400px)";
+}
+    
     
