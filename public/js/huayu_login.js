2973ad3b573226d5e9393818e0dcc71cda5459c1
gejun <476377863@qq.com>
2019-08-01 20:41:02 +0800

    xiuagi

--- public/js/huayu_login.js
+++ public/js/huayu_login.js
@@ -2,7 +2,7 @@ function check_uname (){
     var $uname=uname.value;
     if($uname==""){
         msg1.innerHTML="用户名不能为空！";
-    }else if($uname.length>=6 && $uname.length<=12){
+    }else if($uname.length>=3 && $uname.length<=12){
         msg1.innerHTML="格式正确！";
     }else{
         msg1.innerHTML="格式错误！";
